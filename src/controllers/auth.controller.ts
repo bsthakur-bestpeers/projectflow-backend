9afad526de7458c48cd678590ca078c396c8c362
Bharat Singh Thakur <bsthakur@bestpeers.in>
2026-09-22 18:08:03 +0530

    s

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,7 +5,7 @@ import { JWT_COOKIE_NAME } from "../constants/app.constants";
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax" as const,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : ("lax" as const),
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
